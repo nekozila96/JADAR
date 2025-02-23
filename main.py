88d8ed0cc41b72c274e9870b777c118086a3ecbb
nekozila96 <theanhnguyenk102@gmail.com>
2025-02-24 02:33:11 +0700

    Fix main

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from semgrep import clone_github_repo
 import tempfile
 
 def main():
+    home_dir = "/home/kun"
     github_url = input("Enter the URL of the GitHub repository you want to clone:")
-    local_path = os.getcwd()
+    repo_name = github_url.split("/")[-1]
+    local_path = os.path.join(home_dir, repo_name)
     clone_github_repo(github_url, local_path)
     print(f"Repository cloned to {local_path}")
