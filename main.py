d5033edec0dd9102c5c6ac586fcbe7d20acb6b18
nekozila96 <theanhnguyenk102@gmail.com>
2025-02-24 02:28:34 +0700

    Fix main

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import tempfile
 
 def main():
     github_url = input("Enter the URL of the GitHub repository you want to clone:")
-    local_path = "/home/kun"
-    with tempfile.TemporaryDirectory() as local_path:
-        clone_github_repo(github_url, local_path)
-        print(f"Repository cloned to {local_path}")
+    local_path = os.getcwd()
+    clone_github_repo(github_url, local_path)
+    print(f"Repository cloned to {local_path}")
