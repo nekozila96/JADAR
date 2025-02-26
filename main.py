f55f717190e57849e6b3e66a1211f378ba9db1f3
nekozila96 <theanhnguyenk102@gmail.com>
2025-02-27 02:02:30 +0700

    Fix directory

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     repo_url = input("Please input the URL of Repository you want to test: ")
     repo_name = repo_url.split("/")[-1]
     clone_github_repo(repo_url, repo_name)
-    local_path = os.chdir("f{repo_name}")
+    local_path = os.chdir(f"{repo_name}")
     run_semrep(local_path, repo_name)
 
 
